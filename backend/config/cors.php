c679bf1c484905459a49e1ce051da042545e6856
Pipe Mila <felipemiranda959@gmail.com>
2026-02-24 15:53:26 -0500

    version 2.1

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,20 +6,18 @@ return [
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    // Agregamos la URL de tu proyecto en Vercel a la lista
+    'allowed_origins' => [
+        'http://localhost:5173', 
+        'http://127.0.0.1:5173',
+        'https://ig-ecosystem-git-main-pipe7mirs-projects.vercel.app'
+    ],
 
     'allowed_origins_patterns' => [],
 
